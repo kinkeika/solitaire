62c5e8a9393790f9cfb471ba80ec9906d03aaf24
Brian Tkatch <brian@defectivebit.com>
2016-07-28 08:24:24 -0700

    Add function to convert card number to name
    Added a data conversion function to convert a card number to a friendly
    name to more easily replicate a deck by hand.

--- data-conversion.js
+++ data-conversion.js
@@ -141,5 +141,120 @@ module.exports = {
             default:
                 throw new Error("Invalid number");
         }
+    },
+
+    cardNumberToFriendlyName: function(card) {
+        switch (card) {
+            case 1:
+                return "Ace of Clubs";
+            case 2:
+                return "2 of Clubs";
+            case 3:
+                return "3 of Clubs";
+            case 4:
+                return "4 of Clubs";
+            case 5:
+                return "5 of Clubs";
+            case 6:
+                return "6 of Clubs";
+            case 7:
+                return "7 of Clubs";
+            case 8:
+                return "8 of Clubs";
+            case 9:
+                return "9 of Clubs";
+            case 10:
+                return "10 of Clubs";
+            case 11:
+                return "Jack of Clubs";
+            case 12:
+                return "Queen of Clubs";
+            case 13:
+                return "King of Clubs";
+            case 14:
+                return "Ace of Diamonds";
+            case 15:
+                return "2 of Diamonds";
+            case 16:
+                return "3 of Diamonds";
+            case 17:
+                return "4 of Diamonds";
+            case 18:
+                return "5 of Diamonds";
+            case 19:
+                return "6 of Diamonds";
+            case 20:
+                return "7 of Diamonds";
+            case 21:
+                return "8 of Diamonds";
+            case 22:
+                return "9 of Diamonds";
+            case 23:
+                return "10 of Diamonds";
+            case 24:
+                return "Jack of Diamonds";
+            case 25:
+                return "Queen of Diamonds";
+            case 26:
+                return "King of Diamonds";
+            case 27:
+                return "Ace of Hearts";
+            case 28:
+                return "2 of Hearts";
+            case 29:
+                return "3 of Hearts";
+            case 30:
+                return "4 of Hearts";
+            case 31:
+                return "5 of Hearts";
+            case 32:
+                return "6 of Hearts";
+            case 33:
+                return "7 of Hearts";
+            case 34:
+                return "8 of Hearts";
+            case 35:
+                return "9 of Hearts";
+            case 36:
+                return "10 of Hearts";
+            case 37:
+                return "Jack of Hearts";
+            case 38:
+                return "Queen of Hearts";
+            case 39:
+                return "King of Hearts";
+            case 40:
+                return "Ace of Spades";
+            case 41:
+                return "2 of Spades";
+            case 42:
+                return "3 of Spades";
+            case 43:
+                return "4 of Spades";
+            case 44:
+                return "5 of Spades";
+            case 45:
+                return "6 of Spades";
+            case 46:
+                return "7 of Spades";
+            case 47:
+                return "8 of Spades";
+            case 48:
+                return "9 of Spades";
+            case 49:
+                return "10 of Spades";
+            case 50:
+                return "Jack of Spades";
+            case 51:
+                return "Queen of Spades";
+            case 52:
+                return "King of Spades";
+            case 53:
+                return "Joker A";
+            case 54:
+                return "Joker B";
+            default:
+                throw new Error("Invalid card number");
+        }
     }
 }
